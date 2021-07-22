b07033256d5e2296777110c3fe740ca19c615dc3
nagi125 <nagi125.dev@gmail.com>
2021-07-23 00:47:33 +0900

    新規ユーザー登録無効化

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
 end
